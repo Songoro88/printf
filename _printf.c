b318b5566db28494d049b0e8f0372614dcb49487
Songoro88 <songoro88@gmail.com>
2023-10-18 16:21:48 +0300

    0x11. c - printf alx group project

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - printf function
- * @format: Format.
+ * @format: format.
  * Return: printed characters.
  */
 int _printf(const char *format, ...)
